52e544240cf8380a0f66013d48e4e9564ed0c721
MateoVGarcia <mateovgarcia2003@gmail.com>
2025-08-27 10:58:02 -0300

    bolas

---
+++ src/app/components/main/portfolio/agua-potable/agua-potable.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agua-potable',
+  imports: [],
+  templateUrl: './agua-potable.component.html',
+  styleUrl: './agua-potable.component.css'
+})
+export class AguaPotableComponent {
+
+}
---
+++ src/app/components/main/portfolio/itchio/itchio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-itchio',
+  imports: [],
+  templateUrl: './itchio.component.html',
+  styleUrl: './itchio.component.css'
+})
+export class ItchioComponent {
+
+}
